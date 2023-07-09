9861eeaa86c8892a58515b305e6975c1e0caef5d
Erick <edsghot@gmail.com>
2023-07-09 12:37:25 -0500

    feat: agregando video.routes

---
+++ src/routes/video.routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const VideoC = require('../controllers/videoController')
+
+router.get('/getAll',VideoC.getAll)
+router.post('/insert',VideoC.insert)
+router.get('/getById:id',VideoC.getById)
+router.delete('/delete:id',VideoC.delete)
+
+module.exports = router;
