d1bc07a5910182c46999d32a2c69d22de440a2e2
Erick <edsghot@gmail.com>
2023-07-09 12:27:51 -0500

    feat: agregando videoController

---
+++ src/controllers/videoControlles.js
@@ -0,0 +1,63 @@
+const Video = require("../models/Video")
+
+
+exports.getAll = async (req,res) =>{
+    try{
+        const video = await Video.findAll();
+        res.json({msg: "exitoso",result: video});
+    }catch(error){
+        res.status(500).json({message: 'Error retrieving video',error: error})
+    }
+}
+
+exports.insert = async(req,res)=>{
+    try{
+        const {name,description,url} = req.body;
+        if(!name||!description||!url){
+            res.status(400).json({msg: "uno o mas campos vacios"});
+        }
+        const video = await Video.create({name,description,url});
+
+        res.status(201).json({msg: "operacion exitosa",result:video})
+    }catch(error){
+        res.status(500).json({message: "Error al insertar",error})
+    }
+}
+
+exports.getById = async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const video = await Video.findByPk(id);
+
+        if(!video){
+            return res.status(404).json({msg: "video no encontrado"})
+        }
+
+        res.status(200).json({msg:"exitoso",result:video})
+    }catch(error){
+        res.status(500).json({
+            msg: "error",error:error.message
+        })
+    }
+}
+
+exports.delete = async(req,res)=>{
+    try{
+        const {id} = req.params;
+        
+        const video = await Video.findByPk(id);
+        if(!video){
+            return res.status(404).json({
+                msg: "No se encontro el video"
+            })
+        }
+
+        await video.destroy();
+
+        res.status(200).json({msg:"operacion exitosa"})
+    }catch(error){
+        res.status(500).json({
+            msg: "error",error:error.message
+        })
+    }
+}
