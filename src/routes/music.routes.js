d00215227ddc13bcf66e1508bdb7300f1da65469
Erick <edsghot@gmail.com>
2023-07-09 12:36:00 -0500

    feat: agregando music.routes

---
+++ src/routes/music.routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const musicC = require('../controllers/musicController')
+
+router.get('/getAll',musicC.getAll)
+router.post('/insert',musicC.insert)
+router.get('/getById:id',musicC.getById)
+router.delete('/delete:id',musicC.delete)
+
+module.exports = router;
