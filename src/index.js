f16ffd13322d69b14f003755e134176496a8e3dd
Erick <edsghot@gmail.com>
2023-07-09 12:39:13 -0500

    feat: agregando los routes al index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const userRouter = require('./routes/user.routes');
+const videoRouter = require('./routes/video.routes')
+const musicRouter = require('./routes/video.routes')
 const db = require("./db/database")
 const cors = require("cors");
 const app = express();
@@ -23,6 +25,8 @@ app.use(cors());
 
 
 app.use("/user/",userRouter);
+app.use("/music/",videoRouter);
+app.use("/video/",musicRouter);
 
 app.listen(port,()=>{
     console.log("http://localhost:3030");
