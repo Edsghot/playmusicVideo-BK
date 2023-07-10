6a36caaf605c49ab317463edb9f7bde9e69eadb9
Ed Soto <edsghot@gmail.com>
2023-07-10 08:56:42 -0500

    feat: agregando el controlador al router

--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -4,6 +4,7 @@ const musicC = require('../controllers/musicController')
 router.get('/getAll',musicC.getAll)
 router.post('/insert',musicC.insert)
 router.get('/getById/:id', musicC.getById);
+router.put('/update/:id',musicC.update);
 router.delete('/delete/:id', musicC.delete);
 
 
