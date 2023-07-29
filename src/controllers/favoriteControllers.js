a447e4a9c66192fc37dd823c42fe0624cfd79b45
Ed Soto <edsghot@gmail.com>
2023-07-29 12:51:18 -0500

    fix: Modificando el res de getbyfavorite/video para que retorne un arreglo de json en ves de un arreglo de arreglo de json

--- src/controllers/favoriteControllers.js
+++ src/controllers/favoriteControllers.js
@@ -1,120 +1,130 @@
-const Favorite = require("../models/favorites")
+const Favorite = require("../models/favorites");
 const Video = require("../models/Video");
 
-exports.getAll = async (req,res) =>{
-    try{
-        const favorite = await Favorite.findAll();
-        res.json({msg: "exitoso",result: favorite});
-    }catch(error){
-        res.status(500).json({message: 'Error retrieving Favorite',error: error})
+exports.getAll = async (req, res) => {
+  try {
+    const favorite = await Favorite.findAll();
+    res.json({ msg: "exitoso", result: favorite });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error retrieving Favorite", error: error });
+  }
+};
+
+exports.insert = async (req, res) => {
+  try {
+    let { name, type, idUser, idMusic, idVideo } = req.body;
+
+    idMusic = null;
+
+    if (!idUser || !type || !name) {
+      return res.status(500).json({ msg: "uno o mas campos vacios" });
     }
-}
+    let favorite;
 
-exports.insert = async(req,res)=>{
-    try{
-        let {name,type,idUser,idMusic,idVideo} = req.body;
-        
-        idMusic = null;
+    favorite = await Favorite.create({
+      name: name,
+      type: type,
+      idUser: idUser,
+      idMusic: idMusic,
+      idVideo: idVideo,
+    });
 
-        if(!idUser||!type||!name){
-            return res.status(500).json({msg: 'uno o mas campos vacios'})
-        }
-        let favorite;
+    res.status(201).json({ msg: "operacion exitosa", result: favorite });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error al insertar", error: error.message });
+  }
+};
 
-        favorite = await Favorite.create({name:name,type:type,idUser:idUser,idMusic:idMusic,idVideo:idVideo});
-
-
-        res.status(201).json({msg: "operacion exitosa",result:favorite})
-    }catch(error){
-        res.status(500).json({message: "Error al insertar",error: error.message})
+exports.getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(500).json({ msg: "digite el id" });
     }
-}
+    const favorite = await Favorite.findByPk(id);
 
-exports.getById = async(req,res)=>{
-    try{
-        const {id} = req.params;
-        if(!id){
-            return res.status(500).json({msg:'digite el id'})
-        }
-        const favorite = await Favorite.findByPk(id);
-
-        if(!favorite){
-            return res.status(404).json({msg: "favorite no encontrado"})
-        }
-
-        res.status(200).json({msg:"exitoso",result:favorite})
-    }catch(error){
-        res.status(500).json({
-            msg: "error",error:error.message
-        })
+    if (!favorite) {
+      return res.status(404).json({ msg: "favorite no encontrado" });
     }
-}
 
-exports.delete = async(req,res)=>{
-    try{
-        const {id} = req.params;
-        if(!id){
-            return res.status(500).json({
-                msg: "digite el id"
-            })
-        }
-        const favorite = await Favorite.findByPk(id);
-        if(!favorite){
-            return res.status(404).json({
-                msg: "No se encontro el favorite"
-            })
-        }
+    res.status(200).json({ msg: "exitoso", result: favorite });
+  } catch (error) {
+    res.status(500).json({
+      msg: "error",
+      error: error.message,
+    });
+  }
+};
 
-        await favorite.destroy();
-
-        res.status(200).json({msg:"operacion exitosa"})
-    }catch(error){
-        res.status(500).json({
-            msg: "error",error:error.message
-        })
+exports.delete = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(500).json({
+        msg: "digite el id",
+      });
     }
-}
-
-exports.getByVideo = async(req,res)=>{
-    try{
-        const {idUser,name} = req.query;
-
-        if(!idUser || !name){
-        return res.status(400).json({
-                msg: "uno o mas campos vacios"
-            })
-        }
-
-        const favorites = await Favorite.findAll({
-            where: {
-                name: name,
-                idUser: idUser,
-            },
-            attributes: ['idVideo'],
-        });
-
-        if(!favorites){
-            return res.status(404).json({
-                            msg: "No se encontro el favorite"
-                        })
-        }
-        const idVideoss = favorites.map((favorite) => 
-        favorite.idVideo
-        );
-
-        let Videos = [];
-
-        for(let i = 0;i<idVideoss.length;i++) {
-            Videos.push(await Video.findAll({
-                where: {
-                    id: idVideoss[i],
-                }}))
-        }
-
-        res.status(200).json({msg:"ok",data: Videos})
-
-    }catch(err){
-        res.status(500).json({error:err.message})
+    const favorite = await Favorite.findByPk(id);
+    if (!favorite) {
+      return res.status(404).json({
+        msg: "No se encontro el favorite",
+      });
     }
-    
-}
+
+    await favorite.destroy();
+
+    res.status(200).json({ msg: "operacion exitosa" });
+  } catch (error) {
+    res.status(500).json({
+      msg: "error",
+      error: error.message,
+    });
+  }
+};
+
+exports.getByVideo = async (req, res) => {
+  try {
+    const { idUser, name } = req.query;
+
+    if (!idUser || !name) {
+      return res.status(400).json({
+        msg: "uno o mas campos vacios",
+      });
+    }
+
+    const favorites = await Favorite.findAll({
+      where: {
+        name: name,
+        idUser: idUser,
+      },
+      attributes: ["idVideo"],
+    });
+
+    if (!favorites) {
+      return res.status(404).json({
+        msg: "No se encontro el favorite",
+      });
+    }
+    const idVideoss = favorites.map((favorite) => favorite.idVideo);
+
+    let Videos = [];
+
+    for (let i = 0; i < idVideoss.length; i++) {
+      const videoData = await Video.findAll({
+        where: {
+          id: idVideoss[i],
+        },
+      });
+      // Utilizamos el operador de propagación para agregar los elementos individuales
+      Videos.push(...videoData);
+    }
+
+    res.status(200).json({ msg: "ok", data: Videos });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
